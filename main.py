520dddbfc61eb0e2099028ebc423453777d22591
tdhuynh <tommyhuynh93@gmail.com>
2016-09-26 16:49:57 -0400

    cleaning up code

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 
 rates = {'USD': 1, 'JPY': 100.31, 'EUR': .89, 'BTC': .0016}
 
+
+def conversion_formula(self, other):
+    other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+    return other.amount
+
+
 class Money:
 
     def __init__(self, amount, symbol):
@@ -8,43 +14,45 @@ class Money:
         self.symbol = symbol
 
     def __add__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
-        return self.amount + other.amount, self.symbol
+        conversion_formula(self, other)
+        return self.amount + other.amount
 
     def __mul__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
-        return self.amount * other.amount, self.symbol
+        conversion_formula(self, other)
+        return self.amount * other.amount
 
     def __sub__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
-        return self.amount - other.amount, self.symbol
+        conversion_formula(self, other)
+        return self.amount - other.amount
 
     def __ge__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+        conversion_formula(self, other)
         return self.amount >= other.amount
 
     def __gt__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+        conversion_formula(self, other)
         return self.amount > other.amount
 
     def __le__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+        conversion_formula(self, other)
         return self.amount <= other.amount
 
     def __lt__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+        conversion_formula(self, other)
         return self.amount < other.amount
 
     def __eq__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+        conversion_formula(self, other)
         return self.amount == other.amount
 
     def __ne__(self, other):
-        other.amount = rates[self.symbol]/rates[other.symbol] * other.amount
+        conversion_formula(self, other)
         return self.amount != other.amount
 
-money = Money(20, 'USD')
-money2 = Money(50, 'EUR')
-# money3 = Money(.89,'EUR')
-# money4 = Money(100.31, 'JPY')
-print(money + money2)
+    def __str__(self):
+        return "{} {}".format(self.amount, self.symbol)
+
+money3 = Money(.89, 'EUR')
+money4 = Money(100.31, 'JPY')
+x = Money(20, 'USD') + Money(50, 'EUR')
+print(x)
